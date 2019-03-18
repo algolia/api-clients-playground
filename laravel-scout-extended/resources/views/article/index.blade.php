17f40ae5d8f6fa6b3236aa7aa712db093b052cd8
Tom Klotz <tomklotz.pro@gmail.com>
2019-03-18 10:10:08 +0100

    Add Article Controller, view and route

---
+++ laravel-scout-extended/resources/views/article/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+        <meta charset="UTF-8">
+
+        <title> Article </title>
+
+</head>
+
+<body>
+
+    <h1>All Article</h1>
+</body>
+
+</html>
