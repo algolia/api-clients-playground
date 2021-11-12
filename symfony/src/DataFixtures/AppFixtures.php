37d6d9c33aac9b9f4434b33bcf18f7e5f6ebfb61
Sylvain Bellone <sylvain.bellone@algolia.com>
2021-11-12 19:50:53 +0100

    composer recipes:install doctrine/doctrine-fixtures-bundle --force -v

--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -8,7 +8,7 @@ use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         for ($i = 0; $i < 20; $i++) {
             $article = new Article();
